c494d5b54d7cc8e70bb8100054e2cd7c26446140
Yuyaerawan <164172696+Yuyaerawan@users.noreply.github.com>
2024-07-03 11:57:21 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true },
-  routeRules: {
-    // prerender index route by default
-    '/': { prerender: true },
+export default {
+  head: {
+    title: 'Scenelist',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
   },
-});
+  css: [],
+  components: true,
+  buildModules: [],
+  modules: [],
+  build: {}
+}
