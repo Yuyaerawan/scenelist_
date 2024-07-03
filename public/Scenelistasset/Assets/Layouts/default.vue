13eda81d0757c607eabd1a7f1e6629b21b2bfe97
Yuyaerawan <164172696+Yuyaerawan@users.noreply.github.com>
2024-07-03 11:21:47 +0800

    Add files via upload

---
+++ public/Scenelistasset/Assets/Layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+body {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  background-color: #f8f9fa;
+  color: #343a40;
+  margin: 0;
+  padding: 0;
+}
+</style>
